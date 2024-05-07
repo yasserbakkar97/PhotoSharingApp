0ba92e34f6a03de525160ecc83378bbfc6a8b9a0
YASER BEKER <yasserbakkar97@gmail.com>
2024-05-07 21:26:35 +0300

    Initial commit

---
+++ app/src/main/java/com/example/photosharingapp/UserActivity.kt
@@ -0,0 +1,24 @@
+ package com.example.photosharingapp
+
+import android.os.Bundle
+import android.view.View
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+
+ class UserActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_main)
+
+        fun signIn(view: View){
+
+        }
+
+        fun signUp(view: View){
+
+        }
+
+
+    }
+}
