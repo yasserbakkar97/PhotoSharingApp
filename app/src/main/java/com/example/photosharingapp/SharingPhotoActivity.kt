dfc163c5376487627d22dcd3c1d719d5863bfb0e
YASER BEKER <yasserbakkar97@gmail.com>
2024-05-13 23:27:59 +0300

    Choose Image fun

--- app/src/main/java/com/example/photosharingapp/SharingPhotoActivity.kt
+++ app/src/main/java/com/example/photosharingapp/SharingPhotoActivity.kt
@@ -1,20 +1,86 @@
 package com.example.photosharingapp
 
+import android.Manifest
+import android.content.Intent
+import android.content.pm.PackageManager
+import android.graphics.Bitmap
+import android.graphics.ImageDecoder
+import android.net.Uri
+import android.os.Build
 import android.os.Bundle
+import android.provider.MediaStore
+import android.util.Log
+import android.view.View
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.app.ActivityCompat
+import androidx.core.content.ContextCompat
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 import com.example.photosharingapp.databinding.ActivitySharingPhotoBinding
 
 class SharingPhotoActivity : AppCompatActivity() {
     private lateinit var binding: ActivitySharingPhotoBinding
+    var imageUri: Uri? = null
+    var imageBitmap: Bitmap? = null
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivitySharingPhotoBinding.inflate(layoutInflater)
         enableEdgeToEdge()
         setContentView(binding.root)
 
+    }
+    fun shareImage(view: View) {
 
+
+    }
+    fun chooseImage(view: View) {
+
+        if (ContextCompat.checkSelfPermission(this, Manifest.permission.READ_EXTERNAL_STORAGE ) != PackageManager.PERMISSION_GRANTED){
+
+            ActivityCompat.requestPermissions(this, arrayOf(Manifest.permission.READ_EXTERNAL_STORAGE), 1)
+            Log.d("permission", "permission denied")
+        } else {
+            val intent = Intent(Intent.ACTION_PICK, MediaStore.Images.Media.EXTERNAL_CONTENT_URI)
+            startActivityForResult(intent, 2)
+        }
+    }
+
+    override fun onRequestPermissionsResult(
+        requestCode: Int,
+        permissions: Array<out String>,
+        grantResults: IntArray
+    ) {
+        if (requestCode == 1 ){
+            if (grantResults.size > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED){
+                val intent = Intent(Intent.ACTION_PICK, MediaStore.Images.Media.EXTERNAL_CONTENT_URI)
+                startActivityForResult(intent, 2)
+            }
+        }
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults)
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+
+        if (requestCode == 2 && resultCode == RESULT_OK && data != null){
+
+            imageUri = data.data
+
+            if (imageUri != null){
+
+                if (Build.VERSION.SDK_INT >= 28){
+                    val source = ImageDecoder.createSource(this.contentResolver, imageUri!!)
+                    imageBitmap = ImageDecoder.decodeBitmap(source)
+                    binding.imageView.setImageBitmap(imageBitmap)
+                }
+
+                imageBitmap = MediaStore.Images.Media.getBitmap(this.contentResolver, imageUri)
+                binding.imageView.setImageBitmap(imageBitmap)
+            }
+
+
+        }
+        super.onActivityResult(requestCode, resultCode, data)
     }
 }
